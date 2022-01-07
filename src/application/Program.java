1b81159699eb51c7ae353867e74724386c7d64ae
OrlandoBG <orlandopsn1@gmail.com>
2022-01-07 02:04:45 -0300

    atualização no método main

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,7 @@ import exceptions.BusinessException;
 
 public class Program {
 
-	public static void main(String[] args) throws BusinessException {
+	public static void main(String[] args)  {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
